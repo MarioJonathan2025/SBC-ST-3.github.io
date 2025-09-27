cfcb5b18cb7cbd49d3109e0e19c5909913229d23
MarioJonathan2025 <mario.jmh22@gmail.com>
2025-09-27 15:02:46 -0500

    nuovo proggetto con js

---
+++ js/javascript.js
@@ -0,0 +1,22 @@
+var nombre="pepito"; // Esto es una variable String
+alert(nombre);
+var alumnos=["maria",20,"pepíto",18,"puma",60,"jose jose",80]; // Esto es un Arreglo (Letras y numeros)
+alert(alumnos); 
+var edad=20; // Esto es una variable : guardar numero entero
+alert(edad);
+const  alumnos2={
+   dni: 9999999,
+   nombre : "pepito",
+   celular : 955822155,
+   distrito: "surco",
+   dni2: 88888888,
+   nombre2: "jose jose",
+   celular2: 955888855,
+   distrito2: "sjl",
+   dni3: 666666666,
+   nombre3 : "puma",
+   celular3 : 88558888,
+   distrito3: "Callao",
+};
+alert(alumnos2); // el alert por temas seguridad javascript no te muestra la data pero los object
+console.table(alumnos2); // puedes ver la data de la certus que son todos los registros de los alumnos.
