0a0383e178181a666e2256ba3b6f6f7e4f618e45
John Doe <marioj.mh22@gmail.com>
2025-10-11 21:41:17 -0500

    nuovo html cibo creola

--- assets-criolla/js/main.js
+++ assets-criolla/js/main.js
@@ -82,7 +82,7 @@
 						$('.main.style1')
 							.scrollex({
 								mode:		'middle',
-								delay:		100,
+								delay:		200,
 								initialize:	function() { $(this).addClass('inactive'); },
 								terminate:	function() { $(this).removeClass('inactive'); },
 								enter:		function() { $(this).removeClass('inactive'); },
